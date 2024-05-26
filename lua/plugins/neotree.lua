2f549e8ff5b162681c719e68f3114a69b0f9a90e
Seth Derrick <seth@sethderrick.tech>
2024-05-25 23:56:20 -0500

    Fix neotree bool instead of table error

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,10 +1,19 @@
-print("NeoTree configuration loaded")
-require("neo-tree").setup({
-  filesystem = {
-    filtered_items = {
-      hide_dotfiles = false, -- Show dotfiles
-      hide_gitignored = false,
-      hide_hidden = false, -- Show hidden files
-    },
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  requires = {
+    "nvim-lua/plenary.nvim",
+    "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
   },
-})
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false, -- Show dotfiles
+          hide_gitignored = false,
+          hide_hidden = false, -- Show hidden files
+        },
+      },
+    })
+  end,
+}
